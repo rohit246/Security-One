c5e4a1c7e3d764aa0d5d322e523a4c33a235313b
rohit246 <rohitverma.kgp@gmail.com>
2015-02-09 00:35:11 +0530

    Added Security v2.2.2
    v2.2.2: No trusted nodes, all calculations by normal nodes

---
+++ Security v2.2.2/src/core/ler.java
@@ -0,0 +1,17 @@
+package core;
+import java.util.*;
+
+import movement.MovementModel;
+import movement.Path;
+import routing.MessageRouter;
+import routing.RoutingInfo;
+
+public class ler{
+		public int ut_value;
+		public int time;
+	
+		public ler(int host, int time){
+			this.ut_value = host;
+			this.time = time;
+		}
+}
